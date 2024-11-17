0a654480ef051b266096973530b8c2601bcd7255
ReeFLk <reeflk8@gmail.com>
2024-11-17 20:24:09 +0100

    feat: :rocket: First commit

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Food = () => {
+    
+    const food1 = "Orange";
+    const food2 = "Pizza";
+
+    return(
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+
+export default Food;
