ea474df0a0839cb5ab154168c75da6ec2235258f
ReeFLk <reeflk8@gmail.com>
2024-11-17 21:36:10 +0100

    feat: :sparkles: Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 function App() {
     return (
         <>
-            <Student name="John" age={30} isStudent={true} />
-            <Student name="Jane" age={25} isStudent={false} />
-            <Student name="Marie" age={10} isStudent={true} ></Student>
+            <UserGreeting isLoggedIn={true} username="RomThpt"/>
         </>
     );
 }
