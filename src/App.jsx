b75d95276d05e6d1182230f0a0d6ae02ffd1cb28
ReeFLk <reeflk8@gmail.com>
2024-11-17 22:11:06 +0100

    feat: :sparkles: Using render list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import UserGreeting from "./UserGreeting";
-
+import List from "./List";
 function App() {
+    const fruits = [
+        { id: 1, name: "orange", calories: 45 },
+        { id: 2, name: "kiwi", calories: 45 },
+        { id: 3, name: "pinaeapple", calories: 37 },
+    ];
+
+    fruits.sort((a, b) => a.calories - b.calories);
+
+    const lowCalFruits = fruits.filter((fruit) => fruit.calories < 40);
     return (
         <>
-            <UserGreeting isLoggedIn={true} username="RomThpt"/>
+            {fruits.length > 0 ? (
+                <List items={fruits} category="Fruit" />
+            ) : (
+                <p>No fruits</p>
+            )}
         </>
     );
 }
