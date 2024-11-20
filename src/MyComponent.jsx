817bce04fc1286b28b3cd3a79caee44ffd20044d
ReeFLk <reeflk8@gmail.com>
2024-11-20 22:14:20 +0100

    feat: :sparkles: Using onChange hook

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,27 +1,59 @@
 import { useState } from "react";
-
+/// On change event handler
+// The onChange event handler is used to capture the input value and update the state value. The onChange event handler is used with the input, textarea, and select elements.
 const MyComponent = () => {
-    const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(0);
+    const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("credit");
+    const [shipping, setShipping] = useState("");
 
     return (
         <div>
-            <p>
-                Name: {name}
-                <button onClick={() => setName("RomThpt")}>Update name</button>
-            </p>
-            <p>
-                Age: {age}
-                <button onClick={() => setAge(age + 1)}>Increment</button>
-                <button onClick={() => setAge(age - 1)}>Decrement</button>
-            </p>
-            <p>
-                Employed: {isEmployed ? "Yes" : "No"}
-                <button onClick={() => setIsEmployed(!isEmployed)}>
-                    Toggle employment status
-                </button>
-            </p>
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <p>Name: {name}</p>
+            <input
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                type="number"
+            />
+            <p>Quantity: {quantity}</p>
+            <textarea
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                placeholder="Add some details"
+            />
+            <p>Comment: {comment}</p>
+            <select
+                value={payment}
+                onChange={(e) => setPayment(e.target.value)}
+            >
+                <option>Select an option</option>
+                <option value="Credit">Credit Card</option>
+                <option value="Debit">Debit Card</option>
+                <option value="Paypal">Paypal</option>
+            </select>
+            <p>Payment: {payment}</p>
+            <label>
+                <input
+                    type="radio"
+                    value="Pickup"
+                    checked={shipping === "Pickup"}
+                    onChange={(e) => setShipping(e.target.value)}
+                />
+                Pickup
+            </label>
+            <br />
+            <label>
+                <input
+                    type="radio"
+                    value="Delivery"
+                    checked={shipping === "Delivery"}
+                    onChange={(e) => setShipping(e.target.value)}
+                />
+                Delivery
+            </label>
+            <p>Shipping: {shipping}</p>
         </div>
     );
 };
